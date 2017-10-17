f7137ac05fc24cdc1626d65858ae5a613003f261
Marcus Smith <marcussmith0711@gmail.com>
2017-10-16 20:03:20 -0500

    added POST /users/login route

--- server/models/User.js
+++ server/models/User.js
@@ -85,4 +85,20 @@ userSchema.statics.findByToken = function(token) {
         });
 }
 
+userSchema.statics.findByCredentials = function(email, password) {
+    var User = this;
+
+    return User.findOne({email}).then((user) => {
+        if(!user) return Promise.reject();
+
+        return new Promise((resolve, reject) => {
+            bcrypt.compare(password, user.password, (err, result) => {
+                if(!result) return reject();
+
+                return resolve(user);
+            });
+        });
+    });
+}
+
 module.exports = mongoose.model("User", userSchema);
--- server/server.js
+++ server/server.js
@@ -3,6 +3,7 @@ require("./config/config");
 const express = require("express");
 const bodyParser = require("body-parser");
 const _ = require("lodash");
+const bcrypt = require("bcryptjs");
 const { ObjectID } = require("mongodb");
 
 const { mongoose } = require("./db/mongoose");
@@ -93,6 +94,17 @@ app.post("/users", (req, res) => {
 
 app.get("/users/me", authenticate, (req, res) => {
     res.send(req.user);
+});
+
+app.post("/users/login", (req, res) => {
+    var email = req.body.email;
+    var password = req.body.password;
+
+    User.findByCredentials(email, password).then((user) => {
+        return user.generateAuthToken().then((token) => {
+            res.header('x-auth', token).send(user); 
+        })
+    }).catch(e => res.status(400).send());
 });
 
 app.listen(PORT, () => {

c537c1f6a07ffb9abe673b93e101b98e0cbd12ec
Marcus Smith <marcussmith0711@gmail.com>
2017-10-17 19:34:55 -0500

    added advanced config

--- server/tests/seed/seed.js
+++ server/tests/seed/seed.js
@@ -13,22 +13,28 @@ const users = [{
     password: "useronepassword",
     tokens: [{
         access: "auth",
-        token: jwt.sign({_id: userIdOne, access: "auth"}, "abc123").toString()
+        token: jwt.sign({_id: userIdOne, access: "auth"}, process.env.JWT_SECRET).toString()
     }]
 }, {
     _id: userIdTwo,
     email: "james@example.com",
-    password: "usertwopassword"
+    password: "usertwopassword",
+    tokens: [{
+        access: "auth",
+        token: jwt.sign({_id: userIdTwo, access: "auth"}, process.env.JWT_SECRET).toString()
+    }]
 }];
 
 const todos = [{
     _id: new ObjectID(),
-    text: 'First test todo'
+    text: 'First test todo',
+    _creator: userIdOne
   }, {
     _id: new ObjectID(),
     text: 'Second test todo',
     completed: true,
-    completedAt: 333
+    completedAt: 333,
+    _creator: userIdTwo
   }];
 
 var populateUsers = (done) => {
